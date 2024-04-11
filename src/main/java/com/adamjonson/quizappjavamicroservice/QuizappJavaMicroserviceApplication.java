17c2ccbd49c74a13cc6497372c9f6e4f5a70a307
Hp <adkhamjon.abd@gmail.com>
2024-04-11 09:23:33 +0200

    Initialize the project

---
+++ src/main/java/com/adamjonson/quizappjavamicroservice/QuizappJavaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adamjonson.quizappjavamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappJavaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappJavaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamjonson/quizappjavamicroservice/QuizappJavaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamjonson.quizappjavamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappJavaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
